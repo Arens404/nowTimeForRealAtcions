7c4cc4211e02193e65521124de2454dfa3d9c35e
Arens404 <raczlevente67879@gmail.com>
2022-07-24 14:09:40 +0200

    try fix

--- src/simpleTrianglePatterns/simpleTrianglePattern.java
+++ src/simpleTrianglePatterns/simpleTrianglePattern.java
@@ -3,9 +3,9 @@ package simpleTrianglePatterns;
 import java.util.Scanner;
 public class simpleTrianglePattern {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a number:");
         try{
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a number:");
             int n = scan.nextInt();
             for(int i = 1; i <= n; i++){
                 for(int j = 1; j <= i; j++){
--- src/simpleTrianglePatterns/simpleTrianglePattern2.java
+++ src/simpleTrianglePatterns/simpleTrianglePattern2.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class simpleTrianglePattern2 {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a number:");
         try{
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a number:");
             int n = scan.nextInt();
             for(int i = 1; i <= n; i++){
                 for(int j = i; j <= n; j++){
