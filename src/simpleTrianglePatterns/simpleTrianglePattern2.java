056c94ec7b8e696464171b9e835bc3e5c34f3904
Arens404 <raczlevente67879@gmail.com>
2022-07-24 09:03:12 +0200

    trianglePattern stuff

---
+++ src/simpleTrianglePatterns/simpleTrianglePattern2.java
@@ -0,0 +1,24 @@
+package simpleTrianglePatterns;
+
+import java.util.Scanner;
+
+public class simpleTrianglePattern2 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        try{
+            int n = scan.nextInt();
+            for(int i = 1; i <= n; i++){
+                for(int j = i; j <= n; j++){
+                    System.out.print(" ");
+                }
+                for(int j = 1;j <= i; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong format! Please use numbers to enter the input!");
+        }
+    }
+}
