983a097840b320905e83d7c7848fbf18be6b40b7
Arens404 <raczlevente67879@gmail.com>
2022-07-24 14:20:41 +0200

    try fix 2.0

--- src/simpleTrianglePatterns/simpleTrianglePattern.java
+++ src/simpleTrianglePatterns/simpleTrianglePattern.java
@@ -3,18 +3,18 @@ package simpleTrianglePatterns;
 import java.util.Scanner;
 public class simpleTrianglePattern {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
         try{
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Enter a number:");
             int n = scan.nextInt();
-            for(int i = 1; i <= n; i++){
-                for(int j = 1; j <= i; j++){
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
         } catch (Exception e) {
             System.out.println("Wrong format! Please use numbers to enter the input!");
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
--- src/simpleTrianglePatterns/simpleTrianglePattern2.java
+++ src/simpleTrianglePatterns/simpleTrianglePattern2.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class simpleTrianglePattern2 {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
         try{
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Enter a number:");
             int n = scan.nextInt();
-            for(int i = 1; i <= n; i++){
-                for(int j = i; j <= n; j++){
-                    System.out.print(" ");
-                }
-                for(int j = 1;j <= i; j++){
-                    System.out.print("*");
-                }
-                System.out.println();
-            }
         } catch (Exception e) {
             System.out.println("Wrong format! Please use numbers to enter the input!");
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j <= n; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1;j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
