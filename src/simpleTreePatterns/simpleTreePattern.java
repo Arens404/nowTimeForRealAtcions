81b6545dd1cfb2b6606809f8c65ce9b372c0ebc4
Arens404 <raczlevente67879@gmail.com>
2022-07-24 08:59:29 +0200

    treePattern stuff

---
+++ src/simpleTreePatterns/simpleTreePattern.java
@@ -0,0 +1,39 @@
+package simpleTreePatterns;
+
+import java.util.Scanner;
+
+public class simpleTreePattern {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter two number:");
+        try{
+            int n = scan.nextInt();
+            int b = scan.nextInt();
+            for(int k = 1; k <= 3; k++){
+                for(int i = 1; i <= n; i++){
+                    for(int j = i; j <= n; j++){
+                        System.out.print(" ");
+                    }
+                    for(int j = 1; j <= i; j++){
+                        System.out.print("*");
+                    }
+                    for(int j = 1; j < i; j++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+            }
+            for(int o = 1; o <= b; o++){
+                for(int j = 1; j <= b+1; j++){
+                    System.out.print(" ");
+                }
+                for(int j = 1; j <= b; j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            System.out.println("Wrong format! Please use numbers to enter the input!");
+        }
+    }
+}
