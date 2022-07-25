9eb8425059dbc6b2e64c3cbb5680735546abad2f
Arens404 <raczlevente67879@gmail.com>
2022-07-25 19:04:23 +0200

    Exception fix 10.0

--- src/pinCodeTasks/pinCodeTask1.java
+++ src/pinCodeTasks/pinCodeTask1.java
@@ -1,4 +1,4 @@
-package pinCodeTasks;
+package src.pinCodeTasks;
 
 import java.util.Scanner;
 
@@ -6,33 +6,31 @@ public class pinCodeTask1 {
 
     public static void main(String[] args) {
         try{
-            Scanner scan = new Scanner(System.in);
-            //System.out.print("Enter your pin code:");
-            //int pin = scan.nextInt();
+            Scanner userInputScanner = new Scanner(System.in);
             int truePinCode = 5619;
             int pinTryCount = 2;
-            int realPinTryCount = 3;
+            int PinCodeMonitoringTryCount = 3;
             while(pinTryCount != 5){
-                System.out.print("U only have " + realPinTryCount + " chance\n");
+                System.out.print("U only have " + PinCodeMonitoringTryCount + " chance\n");
                 System.out.print("Enter your pin code:");
-                int pin = scan.nextInt();
+                int userInputPinCode = userInputScanner.nextInt();
                 pinTryCount++;
-                realPinTryCount--;
+                PinCodeMonitoringTryCount--;
                 if(pinTryCount == 5){
                     System.out.println("Phone locked!");
                 }
-                else if(pinTryCount < 5 && pin == truePinCode){
+                else if(pinTryCount < 5 && userInputPinCode == truePinCode){
                     System.out.println("Phone unlocked!");
                     break;
                 }
-                else if(pin <= 999 || pin >= 9999){
+                else if(userInputPinCode <= 999 || userInputPinCode >= 9999){
                     System.out.println("The pin code is four number!");
                     break;
                 }
                 else {
                     System.out.println("Wrong pin!");}
             }
-        }catch (Exception e){
+        }catch (Exception WrongFormatException){
             System.out.println("Wrong format! Please use numbers to enter the input!");
         }
     }
--- src/pinCodeTasks/pinCodeTask2.java
+++ src/pinCodeTasks/pinCodeTask2.java
@@ -1,29 +1,29 @@
-package pinCodeTasks;
+package src.pinCodeTasks;
 
 import java.util.Scanner;
 
 public class pinCodeTask2 {
     public static void main(String[] args) {
         try{
-            Scanner scan = new Scanner(System.in);
+            Scanner userInputScanner = new Scanner(System.in);
             int trueSafeCode = 777777;
-            int safeCodeCount = 0;
+            int safeCodeEnterCount = 0;
 
 
-            while(safeCodeCount != 77777){
+            while(safeCodeEnterCount != 77777){
                 System.out.print("Enter the safe code: ");
-                if(scan.hasNext("q")){
+                if(userInputScanner.hasNext("q")){
                     System.exit(0);
                 } else { }
 
-                int safe = scan.nextInt();
+                int safeCodeInput = userInputScanner.nextInt();
                 {
-                    if(safe == trueSafeCode){
+                    if(safeCodeInput == trueSafeCode){
                         System.out.println("Safe unlocked!");{
                             break;
                         }
                     }
-                    else if(safe <= 99999 || safe >= 999999){
+                    else if(safeCodeInput <= 99999 || safeCodeInput >= 999999){
                         System.out.println("The safe code is six number!");
                         break;
                     }
