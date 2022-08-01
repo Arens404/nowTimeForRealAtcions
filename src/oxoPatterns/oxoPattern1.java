a0cdd236279fa97da4da15347316fea8f50f0195
Arens404 <raczlevente67879@gmail.com>
2022-08-01 23:19:54 +0200

    Classes fixed 2.0

--- src/oxoPatterns/oxoPattern1.java
+++ src/oxoPatterns/oxoPattern1.java
@@ -1,5 +1,6 @@
 package src.oxoPatterns;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class oxoPattern1 {
@@ -7,22 +8,22 @@ public class oxoPattern1 {
         Scanner userInputScanner = new Scanner(System.in);
         try{
             System.out.print("Line:");
-            int userInputNumbersOfLines = userInputScanner.nextInt();
+            int userInputLines = userInputScanner.nextInt();
             System.out.print("Column:");
-            int userInputNumbersOfColumns = userInputScanner.nextInt();
-            for(int i = 1; i <= userInputNumbersOfLines; i++){
+            int userInputColumns = userInputScanner.nextInt();
+            for(int i = 1; i <= userInputLines; i++){
                 if(i % 2 == 0){
-                    for(int j = 1; j <= userInputNumbersOfColumns; j++){
+                    for(int j = 1; j <= userInputColumns; j++){
                         System.out.print("xo");
                     }
                 } else {
-                    for(int j = 1; j <= userInputNumbersOfColumns; j++){
+                    for(int j = 1; j <= userInputColumns; j++){
                         System.out.print("ox");
                     }
                 }
                 System.out.println();
             }
-        } catch (NumberFormatException IllegalArgumentException) {
+        } catch (InputMismatchException IllegalArgumentException) {
             System.out.println("Wrong format! Please use numbers to enter the input!");
         }
     }
--- src/oxoPatterns/oxoPattern2.java
+++ src/oxoPatterns/oxoPattern2.java
@@ -1,24 +1,24 @@
 package src.oxoPatterns;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class oxoPattern2 {
     public static void main(String[] args) {
         try{
             Scanner userInputScanner = new Scanner(System.in);
-
             System.out.print("Line:");
-            int userInputNumbersOfLines = userInputScanner.nextInt();
+            int userInputLines = userInputScanner.nextInt();
             System.out.print("Column:");
-            int userInputNumbersOfColumns = userInputScanner.nextInt();
-            for(int i = 1; i <= userInputNumbersOfLines; i++){
+            int userInputColumns = userInputScanner.nextInt();
+            for(int i = 1; i <= userInputLines; i++){
                 if( i % 3 == 0 || i % 5 == 0 || i % 6 == 0){
-                    for(int j = 1; j <= userInputNumbersOfColumns; j++){
+                    for(int j = 1; j <= userInputColumns; j++){
                         System.out.print("III");
                     }
                 } else{
                     if(i % 2 == 0){
-                        for(int j = 1; j <= userInputNumbersOfColumns; j++){
+                        for(int j = 1; j <= userInputColumns; j++){
                             System.out.print("xo");
                             if(j % 2 == 0){
                                 System.out.print("II");
@@ -26,7 +26,7 @@ public class oxoPattern2 {
                         }
 
                     } else {
-                        for(int j = 1; j <= userInputNumbersOfColumns; j++){
+                        for(int j = 1; j <= userInputColumns; j++){
                             System.out.print("xo");
                             if(j % 2 == 0){
                                 System.out.print("II");
@@ -37,7 +37,7 @@ public class oxoPattern2 {
                 System.out.println();
 
             }
-        } catch (NumberFormatException IllegalArgumentException) {
+        } catch (InputMismatchException IllegalArgumentException) {
             System.out.println("Wrong format! Please use numbers to enter the input!");
         }
     }
