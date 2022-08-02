54607ac34545f03171b8bdb82d68d7250bc8cca3
Arens404 <raczlevente67879@gmail.com>
2022-08-02 20:06:03 +0200

    Class problem fix

--- simpleTreePattern.java
+++ simpleTreePattern.java
@@ -1,14 +1,26 @@
-package simpleTreePatterns;
-
 import java.util.Scanner;
 
-public class simpleTreePattern2 {
-    public static void main(String[] args) {
+public class simpleTreePattern {
+    public static void main(String[] args){
         Scanner userInputScanner = new Scanner(System.in);
         System.out.println("Enter two number:");
         try{
             int userInputTreeTop = userInputScanner.nextInt();
             int userInputTreeBotton = userInputScanner.nextInt();
+            for(int k = 1; k <= 3; k++){
+                for(int i = 1; i <= userInputTreeTop; i++){
+                    for(int j = i; j <= userInputTreeTop; j++){
+                        System.out.print(" ");
+                    }
+                    for(int j = 1; j <= i; j++){
+                        System.out.print("*");
+                    }
+                    for(int j = 1; j < i; j++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+            }
             for(int o = 1; o <= userInputTreeBotton; o++){
                 for(int j = 1; j <= userInputTreeBotton+1; j++){
                     System.out.print(" ");
@@ -18,22 +30,7 @@ public class simpleTreePattern2 {
                 }
                 System.out.println();
             }
-            for(int k = 1; k <= 3; k++){
-                for(int i = 1; i <= userInputTreeTop; i++){
-                    for(int j = 1; j <= i; j++){
-                        System.out.print(" ");
-                    }
-                    for(int j = i; j <= userInputTreeTop; j++){
-                        System.out.print("*");
-                    }
-                    for(int j = i; j < userInputTreeTop; j++){
-                        System.out.print("*");
-                    }
-                    System.out.println();
-                }
-            }
         } catch (NumberFormatException IllegalArgumentException) {
             System.out.println("Wrong format! Please use numbers to enter the input!");
         }
     }
-}
