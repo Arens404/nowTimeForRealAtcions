09c89f96ac39b8fd84d3f96c69771feaa4cafd7d
Arens404 <raczlevente67879@gmail.com>
2022-08-05 17:20:16 +0200

    Variable fix Wrapper type fix

--- Sum.java
+++ Sum.java
@@ -1,38 +1,40 @@
 import java.util.ArrayList;
+import java.util.List;
+
 public class Sum {
     public static void main(String[] args) {
         int[] arrayNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println(Sum.numbersSum(arrayNumbers));
 
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        List numbers = new ArrayList<>();
         int[] inputNumbers = {1, 11, 22, 54, 21, 11, 78};
         for(int i = 0; i < inputNumbers.length; i++){
             numbers.add(inputNumbers[i]);
         }
-        Sum.numbersCheck(numbers);
+        Sum.numbersCheck((ArrayList<Integer>) numbers);
     }
     public static int numbersSum(int[] numbersArray){
-        int inputArrayNumbersSum = 0;
+        int ArrayNumberSum = 0;
         for(int i = 0; i < numbersArray.length; i++){
-            inputArrayNumbersSum += numbersArray[i];
+            ArrayNumberSum += numbersArray[i];
         }
-        return inputArrayNumbersSum;
+        return ArrayNumberSum;
     }
-    public static void numbersCheck(ArrayList<Integer> numbersOfInputArray) {
-        for (int i = 0; i < numbersOfInputArray.size(); i++) {
-            int currentNumber = numbersOfInputArray.get(i);
+    public static void numbersCheck(ArrayList<Integer> inputNumberArray) {
+        for (int i = 0; i < inputNumberArray.size(); i++) {
+            int currentNumber = inputNumberArray.get(i);
             int numberCheckCount = 0;
-            for (int j = 0; j < numbersOfInputArray.size(); j++) {
-                if (currentNumber == numbersOfInputArray.get(j)) {
+            for (int j = 0; j < inputNumberArray.size(); j++) {
+                if (currentNumber == inputNumberArray.get(j)) {
                     numberCheckCount++;
                 }
                 if(numberCheckCount == 2){
-                    numbersOfInputArray.remove(j);
+                    inputNumberArray.remove(j);
                 }
             }
         }
-        for(int i = 0; i < numbersOfInputArray.size(); i++){
-            System.out.println(numbersOfInputArray.get(i));
+        for(int i = 0; i < inputNumberArray.size(); i++){
+            System.out.println(inputNumberArray.get(i));
         }
     }
 }
