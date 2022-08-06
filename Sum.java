2ca63912091c20e7fd8ee963bf672bfbc1ee8a40
Arens404 <raczlevente67879@gmail.com>
2022-08-06 09:48:43 +0200

    Static fix 2.0

--- Sum.java
+++ Sum.java
@@ -1,27 +1,27 @@
-package SumTasks;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Sum {
     public static void main(String[] args) {
+        Sum sum = new Sum();
         int[] arrayNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(Sum.numbersSum(arrayNumbers));
+        System.out.println(sum.numbersSum(arrayNumbers));
 
         List numbers = new ArrayList<>();
         int[] inputNumbers = {1, 11, 22, 54, 21, 11, 78};
         for(int i = 0; i < inputNumbers.length; i++){
             numbers.add(inputNumbers[i]);
         }
-        Sum.numbersCheck((ArrayList<Integer>) numbers);
+        sum.numbersCheck((ArrayList<Integer>) numbers);
     }
-    public static int numbersSum(int[] numbersArray){
+    public int numbersSum(int[] numbersArray){
         int ArrayNumberSum = 0;
         for(int i = 0; i < numbersArray.length; i++){
             ArrayNumberSum += numbersArray[i];
         }
         return ArrayNumberSum;
     }
-    public static void numbersCheck(ArrayList<Integer> inputNumberArray) {
+    public void numbersCheck(ArrayList<Integer> inputNumberArray) {
         for (int i = 0; i < inputNumberArray.size(); i++) {
             int currentNumber = inputNumberArray.get(i);
             int numberCheckCount = 0;
