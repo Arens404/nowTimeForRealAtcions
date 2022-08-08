3bd6bec267b9c9dd1d9a723675772107a49f8b14
Arens404 <raczlevente67879@gmail.com>
2022-08-08 14:20:58 +0200

    Scanner fix

--- oxoPattern1.java
+++ oxoPattern1.java
@@ -1,13 +1,13 @@
-import oxoPatternTasksMain.Setter;
+import java.util.Scanner;
 
 public class oxoPattern1{
-    private Setter scanner= new Setter();
 
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         oxoPattern1 TasksFromChezo = new oxoPattern1();
         System.out.print("Select witch task u wanna try:");
         try{
-            int UserInputSelectionInt = TasksFromChezo.scanner.input();
+            int UserInputSelectionInt = scanner.nextInt();
             switch (UserInputSelectionInt){
                 case 1 -> TasksFromChezo.oxoPatternTaskOne();
                 case 2 -> TasksFromChezo.oxoPatternTaskTwo();
@@ -20,9 +20,9 @@ public class oxoPattern1{
     public void oxoPatternTaskOne(){
         try{
             System.out.print("Line:");
-            int userInputLines = scanner.input();
+            int userInputLines = scanner.nextInt();
             System.out.print("Column:");
-            int userInputColumns = scanner.input();
+            int userInputColumns = scanner.nextInt();
             for(int i = 1; i <= userInputLines; i++){
                 for(int j = 1; j <= userInputColumns; j++){
                     if(i % 2 == 0){
@@ -40,9 +40,9 @@ public class oxoPattern1{
     public void oxoPatternTaskTwo(){
         try{
             System.out.print("Line:");
-            int userInputLines = scanner.input();
+            int userInputLines = scanner.nextInt();
             System.out.print("Column:");
-            int userInputColumns = scanner.input();
+            int userInputColumns = scanner.nextInt();
             for(int i = 1; i <= userInputLines; i++){
                 if( i % 3 == 0 || i % 5 == 0 || i % 6 == 0){
                     for(int j = 1; j <= userInputColumns; j++){
