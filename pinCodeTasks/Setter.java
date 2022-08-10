b32293db2afa9bcc74f8148f6274948a8b950646
Arens404 <raczlevente67879@gmail.com>
2022-08-10 13:34:22 +0200

    Setter class deleted

--- pinCodeTasks/Setter.java
+++
@@ -1,8 +0,0 @@
-package pinCodeTasks;
-public class Setter {
-    private java.util.Scanner scanner = new java.util.Scanner(System.in);
-    public int input() {
-        int input = scanner.nextInt();
-        return input;
-    }
-}
