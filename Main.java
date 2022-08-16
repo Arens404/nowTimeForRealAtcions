ea444c4ed867e909d7374a2014ee6e7adc566a7b
Arens404 <raczlevente67879@gmail.com>
2022-08-16 17:54:07 +0200

    Hash Tasks completed

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,52 +12,21 @@ public class Main {
         telephoneNumbers.put("Brooke P. Askew", "307-687-2982");
         System.out.print("Select key:");
         String keySelect = scanner.nextLine();
-        switch (keySelect){
-            case "William A. Lathan" -> {
-                System.out.println("PhoneNumber or Name? (P/N)");//2
-                if(scanner.hasNext("P")){
-                    System.out.println(telephoneNumbers.get("William A. Lathan"));
-                } else if (scanner.hasNext("N")) {
-                    System.out.println((telephoneNumbers.keySet().toArray()[2]));
+        System.out.println("(Press P/N)");
+        if (scanner.hasNext("P")) {
+            System.out.println(telephoneNumbers.get("Phone Number: " + keySelect));
+        } else if (scanner.hasNext("N")) {
+            String value = telephoneNumbers.get(keySelect);
+            for(Entry<String,String> entry: telephoneNumbers.entrySet()){
+                if(entry.getValue() == value){
+                    System.out.println("Name: " + entry.getKey());
                 }
             }
-            case "John K. Miller" -> {
-                System.out.println("PhoneNumber or Name? (P/N)");
-                if(scanner.hasNext("P")){
-                    System.out.println(telephoneNumbers.get("John K. Miller"));//0
-                } else if (scanner.hasNext("N")) {
-                    System.out.println((telephoneNumbers.keySet().toArray()[0]));
-                }
-            }
-            case "Hortensia E. Foster" -> {
-                System.out.println("PhoneNumber or Name? (P/N)");
-                if(scanner.hasNext("P")){
-                    System.out.println("Hortensia E. Foster");
-                } else if (scanner.hasNext("N")) {
-                    System.out.println((telephoneNumbers.keySet().toArray()[4]));
-                }
-            }
-            case "Amanda D. Newland" -> {
-                System.out.println("PhoneNumber or Name? (P/N)");
-                if(scanner.hasNext("P")){
-                    System.out.println(telephoneNumbers.get("Amanda D. Newland"));
-                } else if (scanner.hasNext("N")) {
-                    System.out.println((telephoneNumbers.keySet().toArray()[3]));
-                }
-
-            }
-            case "Brooke P. Askew" -> {
-                System.out.println("PhoneNumber or Name? (P/N)");
-                if(scanner.hasNext("P")){
-                    System.out.println(telephoneNumbers.get("Brooke P. Askew"));//1
-                } else if (scanner.hasNext("N")) {
-                    System.out.println((telephoneNumbers.keySet().toArray()[1]));
-                }
-
-            }
-            default -> {
-                System.out.println("I dont have this phone number and name!");
-            }
+        } else {
+            System.out.println("I dont have this phone number and name!");
         }
     }
 }
+
+
+
