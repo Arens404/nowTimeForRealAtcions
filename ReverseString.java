1abc4bb2a08aee244e77fa32c2011093d148bf3d
Arens404 <raczlevente67879@gmail.com>
2022-08-20 13:12:07 +0200

    Map tasks completed

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        {
+            String testString = "Have a nice day my friend";
+            char[] result = ReverseString(testString);
+            System.out.println(result);
+            //ReverseString(testString);
+        }
+    }
+    public static char[] ReverseString(String userString){
+        int length = userString.length()-1;
+        char[] block = new char[length+1];
+        for(int i = 0; i < userString.length();i++){
+            block[i] = (userString.charAt(length-i));
+        }
+        return block;
+    }
+}
