86ecb7489492f53ec43ef24f2bcb054b4a4c90d6
Arens404 <raczlevente67879@gmail.com>
2022-08-24 14:15:01 +0200

    Control panel now is user-friendly!

--- Main.java
+++ Main.java
@@ -12,11 +12,10 @@ public class Main {
         FirstHashMap.put("978-1-60309-461-0", "The Lab");
         System.out.println("1 - out print the map values and keys");
         System.out.println("2 - remove an element from the map");
-        System.out.println("3 - remove another element from the map");
-        System.out.println("4 - put two element in to the map");
-        System.out.println("5 - Is there a specific value associated with the key?");
+        System.out.println("4 - put two an element in to the map");
+        System.out.println("5 - key value check");
         System.out.println("6 - Get a key value");
-        System.out.println("Select an option (1-7):");
+        System.out.println("Select an option (1-6):");
         int selection = scanner.nextInt();
         switch(selection){
             case 1 -> {
@@ -25,24 +24,32 @@ public class Main {
                 }
             }
             case 2 -> {
-                FirstHashMap.remove("978-1-60309-444-3", "Red Panda and Moon Bear");
-            }
-            case 3 -> {
-                FirstHashMap.remove("978-1-60309-461-0","The Lab");
+                System.out.println("Enter the key that you wanna delete:");
+                String deleteThisKey = scanner.nextLine();
+                System.out.println("Enter the value that you wanna delete");
+                String deleteThisValue = scanner.nextLine();
+                FirstHashMap.remove(deleteThisKey, deleteThisValue);
             }
             case 4 -> {
-                FirstHashMap.put("978-1-60309-450-4", "They Called Us Enemy");
-                FirstHashMap.put("978-1-60309-453-5", "Why Did We Trust Him?");
+                System.out.println("Enter the key that you wanna add:");
+                String addThisKey = scanner.nextLine();
+                System.out.println("Enter the value that you wanna add");
+                String addThisValue = scanner.nextLine();
+                FirstHashMap.put(addThisKey, addThisValue);
             }
             case 5 -> {
-                if(FirstHashMap.containsValue(FirstHashMap.get("478-0-61159-424-8"))){
+                System.out.println("Enter the key that you wanna check:");
+                String keyCheck = scanner.nextLine();
+                if(FirstHashMap.containsValue(FirstHashMap.get(keyCheck))){
                     System.out.println("True");
                 } else {
                     System.out.println("False");
                 }
             }
             case 6 -> {
-                System.out.println(FirstHashMap.get("978-1-60309-453-5"));
+                System.out.println("Enter the key that you wanna print out value:");
+                String printOutThisValue = scanner.nextLine();
+                System.out.println(FirstHashMap.get(printOutThisValue));
             }
         }
     }
