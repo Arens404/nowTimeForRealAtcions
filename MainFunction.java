837e58d3f67f465d23bd92a6bff5b6db1c518085
Arens404 <raczlevente67879@gmail.com>
2022-08-26 14:23:44 +0200

    No problems.

---
+++ MainFunction.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MainFunction { final static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    System.out.println("Enter a number to divide it:");
+    double divideThisNumber = scanner.nextInt();
+    System.out.println(dividerFunction(divideThisNumber));
+  }
+
+  public static double dividerFunction(double dividedNum) {
+    double result = 0;
+    if (dividedNum == 0) {
+      System.out.println("Error! I Can't divide this number!");
+    } else {
+      result = 10 / dividedNum;
+    }
+    return result;
+  }
+}
+
