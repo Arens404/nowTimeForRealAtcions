6b86d161f8ef1ec3a5aff43fbffe750dfec7d91e
Arens404 <raczlevente67879@gmail.com>
2022-10-30 12:20:25 +0100

    File Reader Task completed!

--- FileReadFunction.java
+++ FileReadFunction.java
@@ -1,26 +1,55 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class FileReadFunction { public static Path myFilePath;
 
-  public static final Scanner scanner = new Scanner(System.in);
+  static Map<Integer, Integer> NumberRate = new HashMap<>(3065);
+
+  static void fillUpMap(String number){
+      int lotteryNumber = Integer.parseInt(number);
+      if(NumberRate.containsKey(lotteryNumber)) {
+        NumberRate.replace(lotteryNumber, NumberRate.get(lotteryNumber) + 1);
+      } else {
+        NumberRate.put(lotteryNumber, 1);
+      }
+  }
 
   public static void main(String[] args) {
-    System.out.println("Enter your file name:");
-    String userFileName = scanner.nextLine();
-    myFilePath = Paths.get(userFileName);
+    myFilePath = Paths.get("myTestDoc.txt");
+
+
+    String line = ",";
+    String[] oneLine = new String[3065];
+
     try {
-      List<String> content = Files.readAllLines(myFilePath);
-      for (int i = 0; i < content.size(); i++) {
-        System.out.println(content.get(i));
+      BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(myFilePath)));
+      while((line = reader.readLine()) != null) {
+        oneLine = line.split(";");
+
+        fillUpMap(oneLine[11]);
+        fillUpMap(oneLine[12]);
+        fillUpMap(oneLine[13]);
+        fillUpMap(oneLine[14]);
+        fillUpMap(oneLine[15]);
+      } reader.close();
+
+      int max = 0;
+      int maxNum = 0;
+
+      for(Integer numbers: NumberRate.keySet()){
+        if(NumberRate.get(numbers) > max){
+          max = NumberRate.get(numbers);
+          maxNum = numbers;
+        }
       }
-    } catch (NoSuchFileException NoSuchFileException) {
-      System.out.print("Unable to read file: " + myFilePath.getFileName());
+
+      System.out.println(max + "  " + maxNum);
+
     } catch (IOException e) {
       e.printStackTrace();
     }
