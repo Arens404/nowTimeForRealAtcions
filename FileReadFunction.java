d7b6dc0e2e194b4ac530048b82a6f18df88333c9
Arens404 <raczlevente67879@gmail.com>
2022-11-01 12:45:14 +0100

    File Reader Task completed!

--- FileReadFunction.java
+++ FileReadFunction.java
@@ -8,70 +8,56 @@ import java.util.Map.Entry;
 
 public class FileReadFunction { public static Path myFilePath;
 
-  static Map<Integer, Integer> NumberRate = new HashMap<>(3065);
-  static Map<Integer, Integer> NumberRates = new HashMap<>(3065);
+  static Map<Integer, Integer> NumberRate = new HashMap<>(100);
 
   static void fillUpMap(String number){
-    /*
-    for(int i = 0; i < count; i++){
-      NumberRate.put(lotteryNumberOne[i] + lotteryNumberTwo[i] + lotteryNumberThree[i] + lotteryNumberFour[i] + lotteryNumberFive[i], 1);
+    int lotteryNumber = Integer.parseInt(number);
+    if(NumberRate.containsKey(lotteryNumber)) {
+      NumberRate.replace(lotteryNumber, NumberRate.get(lotteryNumber) + 1);
+    } else {
+      NumberRate.put(lotteryNumber, 1);
     }
-    int startIndex = 0;
-    for (int i = startIndex; i < NumberRate.size(); i++) {
-      for (int j = 0; j < count; j++) {
-        int Numbers = lotteryNumberOne[j] + lotteryNumberTwo[j] + lotteryNumberThree[j] + lotteryNumberFour[j] + lotteryNumberFive[j];
-        if (NumberRate.get(i).equals(Numbers)) {
-          System.out.println("uwu");
-        }
-      }
-    }
-
-     */
   }
 
   public static void main(String[] args) {
-    myFilePath = Paths.get("igen.txt");
+    myFilePath = Paths.get("myTestDoc.txt");
 
     String line = ",";
     String[] oneLine = new String[3065];
-    int[] lotteryNumberOne = new int[3065];
-    int[] lotteryNumberTwo = new int[3065];
-    int[] lotteryNumberThree = new int[3065];
-    int[] lotteryNumberFour = new int[3065];
-    int[] lotteryNumberFive = new int[3065];
-    int count = 0;
+
     try {
       BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(myFilePath)));
       while((line = reader.readLine()) != null) {
         oneLine = line.split(";");
-        lotteryNumberOne[count] = Integer.parseInt(oneLine[11]);
-        lotteryNumberTwo[count] = Integer.parseInt(oneLine[12]);
-        lotteryNumberThree[count] = Integer.parseInt(oneLine[13]);
-        lotteryNumberFour[count] = Integer.parseInt(oneLine[14]);
-        lotteryNumberFive[count] = Integer.parseInt(oneLine[15]);
-        count++;
+
+        fillUpMap(oneLine[11]);
+        fillUpMap(oneLine[12]);
+        fillUpMap(oneLine[13]);
+        fillUpMap(oneLine[14]);
+        fillUpMap(oneLine[15]);
+
       } reader.close();
-
-
-      for(int i = 0; i < count; i++){
-        NumberRate.put(lotteryNumberTwo[i], 1);
+/*
+      System.out.println("Number rates: ");
+      for (Integer item: NumberRate.values()) {
+        System.out.println(item);
       }
+*/
 
-      int startIndex = 0;
-      for (int i = startIndex; i < NumberRate.size(); i++) {
-        for (int j = 0; j < 5; j++) {
-          Integer Numbers = lotteryNumberTwo[j];
-          if (Numbers.equals(NumberRate.get(i))) {
-              NumberRates.put(Numbers,NumberRates.get(i)+1);
-          } else {
-            NumberRates.put(Numbers, 1);
-          }
+      int max = 0;
+      ArrayList<Integer> fiveBestNum = new ArrayList<>();
+      for(Integer numbers: NumberRate.keySet()){
+        if(NumberRate.get(numbers) > max){
+          max = NumberRate.get(numbers);
+          fiveBestNum.add(NumberRate.get(numbers));
         }
       }
 
-      for (Entry<Integer, Integer> item: NumberRates.entrySet()) {
-        System.out.println("Key: " + item.getKey() + " | Value: " + item.getValue());
+      System.out.println("idk");
+      for(int i = 0; i < 5; i++){
+        System.out.println(fiveBestNum.get(i));
       }
+
 
     } catch (IOException e) {
       e.printStackTrace();
